6c7ac74dfd26e1f6b598522341461c1fab231589
Noshan1 <noshanhaider73@gmail.com>
2025-02-18 01:26:58 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Magsi',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Noshan Haider",
+  botOwner: 'Noshan Haider', // Your name
+  ownerNumber: '923297627399', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
